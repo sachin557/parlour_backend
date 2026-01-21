6b43b3858da5bf4c831b5a499cec9203ac0ee1a2
sachin557 <sachinprabhu557@gmail.com>
2026-01-21 13:23:42 +0530

    updated

--- git_push.ps1
+++ git_push.ps1
@@ -8,6 +8,21 @@ Start-sleep -Seconds 5
 if ($LASTEXITCODE -ne 0){
 write-host "Error occured while git push:`n"
 write-host $git_push
+write-host "==============================================================================================================="
+write-host "`n"
+write-host "trying the other meothod to push"
+start-sleep -seconds 3
+git pull origin main
+start-sleep -seconds 5
+$git_pull_push=git push origin main
+start-sleep -seconds 5
+if ($LASTEXITCODE -ne 0){
+    write-host "Error occured while git push:`n"
+write-host $git_pull_push
+}
+else{
+    write-host "push is Completed successfully"
+}
 }
 else{
 write-host "push is successfull"
