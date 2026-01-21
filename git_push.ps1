ab9450f709a459f070d171c4eecbcc37e3d13664
sachin557 <sachinprabhu557@gmail.com>
2026-01-21 13:15:40 +0530

    updated

--- git_push.ps1
+++ git_push.ps1
@@ -3,7 +3,7 @@ Start-Sleep -Seconds 5
 $commit_name=read-host "enter the Commit"
 git commit -m $commit_name
 Start-Sleep -Seconds 5
-$git_push= git push -u origin main
+$git_push=git push -u origin main 2>&1
 Start-sleep -Seconds 5
 if ($git_push -contains "error"){
 write-host "Error occured while git push:`n"
